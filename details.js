b702a11a2abdb2362052b0cb7097efe1625cbf0b
Mauro Schork <mauroschork107@hotmail.com>
2022-08-08 16:17:55 -0300

    agregue los datos de la api, falta arreglar details y stats

--- details.js
+++ details.js
@@ -1,11 +1,15 @@
 const api = "https://amazing-events.herokuapp.com/api/events";
-let id = parseInt(new URLSearchParams(location.search).get('id'));
-const persona = api.events.find(item => item._id == id)
-console.log(persona)
+fetch(api)
+    .then(response => response.json())
+    .then(data => mainArray(data))
+const mainArray = (data) => {
+    let id = new URLSearchParams(location.search).get('id');
+    const persona = data.events.find(item => item._id == id)
+    console.log(persona)
 
-const div = document.getElementById("containerDetail")
-div.innerHTML =
- ` <div class="container card" height="60%" width="70%" >
+    const div = document.getElementById("containerDetail")
+    div.innerHTML =
+        ` <div class="container card" height="60%" width="70%" >
 <img src="${persona.image}" class="container" style="border-solid-dark" alt="..." width="280px" height="200px" </img>
                            <h5 class="card-title"> ${persona.name}</h5>
                            <p class="card-text"> Date: ${persona.date}</p>
@@ -13,8 +17,9 @@ div.innerHTML =
                            <p class="card-text"> Category: ${persona.category}</p>
                            <p class="card-text"> Place: ${persona.place}</p>
                            <p class="card-text"> Capacity: ${persona.capacity}</p>
-                           <p class="card-text"> Assistance/Estimate: ${persona.assistance ? persona.assistance: persona.estimate}</p>  
+                           <p class="card-text"> Assistance/Estimate: ${persona.assistance ? persona.assistance : persona.estimate}</p>  
                            <p>Price: ${persona.price}</p>
                            <a href="./details.html?id=${persona._id}" class="btn btn-primary btnCard">Buy</a>
                            </div>
                            `
+}
